25dcc69b4426134405fd9e1e82f0a9739d4e63de
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-12-27 23:15:27 +0600

    final done

---
+++ Integration Testing/AutoTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace ITesting
+{
+    [TestFixture]
+    public class AutoTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://elp.duetbd.org/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("username")).SendKeys("170042019");
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("Abc.1234");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            driver.FindElement(By.XPath("//img[@alt='ELP']")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://elp.duetbd.org/theme/image.php/academi/core/1606326980/u/f2_white')]")).Click();
+            driver.FindElement(By.Id("actionmenuaction-6")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ Integration Testing/ITesting.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration_Testing
+{
+    class ITesting
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://elp.duetbd.org";
+            IWebElement element1 = driver.FindElement(By.XPath("//*[@id='header']/div/ul/li[2]/div/span/a"));
+            element1.Click();
+            IWebElement element = driver.FindElement(By.Name("username"));
+            element.SendKeys("170042019");
+            IWebElement password = driver.FindElement(By.Name("password"));
+            password.SendKeys("Abc.1234");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            String at = driver.Title;
+
+            String et = "Dashboard";
+            if (at == et)
+            {
+                Console.WriteLine("Test Successful");          
+            }
+            else
+            {
+                Console.WriteLine("Unsuccessful");             
+            }
+            
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
